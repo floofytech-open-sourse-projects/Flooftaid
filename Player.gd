e9fab0cf7ebc3a5583be403c54edf24c5ee44ed5
floofytech-public-projects <floofytechf0@gmail.com>
2026-04-20 04:20:49 -0700

    Implement Player.gd

---
+++ Player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+var speed = 400
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+    velocity = Vector2.ZERO
+    if Input.is_action_pressed("ui_right"): 
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"): 
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"): 
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"): 
+        velocity.y -= 1
+
+    velocity = velocity.normalized() * speed
+    move_and_slide(velocity)
